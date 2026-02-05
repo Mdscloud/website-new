6b716471b698e9e736a14166b0b449df2e581698
Rodrigo Macegossa <rodrigo@promovase.net>
2026-02-05 16:57:55 -0300

    Atualizando adopt

--- src/components/CookieConsent.tsx
+++ src/components/CookieConsent.tsx
@@ -2,20 +2,30 @@ import { useEffect } from "react";
 
 export function CookieConsent() {
   useEffect(() => {
+    // Adiciona a meta tag do Adopt
+    const metaTag = document.createElement("meta");
+    metaTag.name = "adopt-website-id";
+    metaTag.content = "6459de6a-390d-4c47-a9bd-c2231cb83b1d";
+    document.head.appendChild(metaTag);
+
     // Carrega o script do Adopt
     const script = document.createElement("script");
-    script.src = "https://cdn.goadopt.io/bundle.js";
+    script.src = "//tag.goadopt.io/injector.js?website_code=6459de6a-390d-4c47-a9bd-c2231cb83b1d";
+    script.className = "adopt-injector";
     script.async = true;
-    script.setAttribute("data-site-id", "6459de6a-390d-4c47-a9bd-c2231cb83b1d");
     
     // Adiciona o script ao head
     document.head.appendChild(script);
 
-    // Cleanup: remove o script quando o componente for desmontado
+    // Cleanup: remove o script e meta tag quando o componente for desmontado
     return () => {
-      const existingScript = document.querySelector('script[src="https://cdn.goadopt.io/bundle.js"]');
+      const existingScript = document.querySelector('script.adopt-injector');
       if (existingScript) {
         document.head.removeChild(existingScript);
+      }
+      const existingMeta = document.querySelector('meta[name="adopt-website-id"]');
+      if (existingMeta) {
+        document.head.removeChild(existingMeta);
       }
     };
   }, []);
